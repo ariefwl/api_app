6bbdd040e24e7d490952b0a3ba07c75ca01c33ff
ariefwl <ariefwl79@gmail.com>
2025-12-10 20:34:24 +0700

    setting middleware cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+         // Aktifkan CORS middleware untuk semua route API
+        $middleware->api(prepend: [
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
